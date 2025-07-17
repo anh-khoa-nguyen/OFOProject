45698301750fa0c66dd2a9cfc6c5c97c490a279e
anh-khoa-nguyen <nak.khoanguyen@gmail.com>
2025-07-17 15:18:29 +0700

    update UI trangchu + timkiemnhahhang

--- OFO/index.py
+++ OFO/index.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route("/tim-kiem")
+def tim_kiem():
+    return render_template('tim-kiem.html')
+
 if __name__ == '__main__':
     with app.app_context():
         app.run(debug=True)
