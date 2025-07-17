be741e87d892d4b5ecac2574a0b815be5e904f0b
HoYenMi <hoyenmi0603@gmail.com>
2025-07-17 15:27:06 +0700

    UI dang ki-dang nhap nha hang

--- OFO/index.py
+++ OFO/index.py
@@ -4,8 +4,14 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template('Restaurant/ResLogin.html')
+@app.route('/reslogin')
+def login():
+    return render_template('Restaurant/ResLogin.html')
 
+@app.route('/resregister')
+def register():
+    return render_template('Restaurant/ResRegister.html')
 if __name__ == '__main__':
     with app.app_context():
         app.run(debug=True)
